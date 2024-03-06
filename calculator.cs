bed2df13ba136f23fa4f757622f62e64e20d6574
MastayY <nasywanfadilah@gmail.com>
2024-03-06 16:54:20 +0700

    feat: menambahkan fungsi pengurangan

--- calculator.cs
+++ calculator.cs
@@ -1,9 +1,17 @@
 class Calculator {
 	static void Main(string[] args) {
+		int a = 10;
+		int b = 9;
 		
+		Console.WriteLine("Hasil penjumlahan angka {0} dan {1} adalah {2}", a, b, sum(a, b));
+		Console.WriteLine("Hasil pengurangan angka {0} dan {1} adalah {2}", a, b, min(a, b));
 	}
 	
 	static int sum(int a, int b) {
 		return a + b;
+	}
+	
+	static int min(int a, int b) {
+		return a - b;
 	}
 }
